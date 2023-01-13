74a00014d7d435c9dfe1eb5033efbb0df5e5416f
qkrwngud <105642704+qkrwngud@users.noreply.github.com>
2023-01-13 22:07:14 +0900

    2023.1.13

---
+++ 3-1.java
@@ -0,0 +1,17 @@
+public class Main {
+	
+	static void lending() {System.out.println("비행기가 이륙한다.");}
+	static void flying() {System.out.println("비행기가 비행을 하고 있다.");}
+	static void takeoff() {System.out.println("비행기가 착륙한다.");}
+	
+	
+	public static void main(String[] args) {
+		
+		lending();
+		flying();
+		takeoff();
+		
+		
+	}
+
+}
